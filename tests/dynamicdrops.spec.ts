c4296c141a7bf55266af4cfbd477d12b3c85c26a
Gangadhar Dasari <is190302@rguktn.ac.in>
2026-02-28 09:03:16 +0530

    change in config

--- tests/dynamicdrops.spec.ts
+++ tests/dynamicdrops.spec.ts
@@ -1,16 +1,15 @@
 import {test,expect,Locator} from '@playwright/test';
 
 
-
-test('verify dynamic Dropdown',async({page})=>{
-    await page.goto("https://www.flipkart.com/");
-    await page.locator("//input[@name='q']").first().fill('smart');
-   // await page.locator("//input[@title='Search for products, brands and more']").first().fill('smart');
-    // await page.getByRole('textbox', { name: 'Search for Products, Brands' }).fill('smart');
-    await page.waitForTimeout(5000);
-    const searchrelev:number=await page.locator("ul li a").count();
-    const searchreleva:any []=await page.locator("ul li a").allTextContents();
-    console.log(searchrelev);
-    console.log(searchreleva);
+// test('verify dynamic Dropdown',async({page})=>{
+//     await page.goto("https://www.flipkart.com/");
+//     await page.locator("//input[@name='q']").first().fill('smart');
+//    // await page.locator("//input[@title='Search for products, brands and more']").first().fill('smart');
+//     // await page.getByRole('textbox', { name: 'Search for Products, Brands' }).fill('smart');
+//     await page.waitForTimeout(5000);
+//     const searchrelev:number=await page.locator("ul li a").count();
+//     const searchreleva:any []=await page.locator("ul li a").allTextContents();
+//     console.log(searchrelev);
+//     console.log(searchreleva);
     
-})
+// })
